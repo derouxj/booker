6ba47dd0ea2ac3bb472c7130f6141563ec3e3d43
DEROUX Julien <derouxjulien@gmail.com>
2015-11-20 13:13:50 +0100

    Merge origin/master
    Conflicts:
    	nbproject/project.properties
    	nbproject/project.xml

---
+++ data/create.sql
@@ -0,0 +1,11 @@
+CREATE TABLE utilisateur (
+    id STRING primary key,
+    avatar STRING,
+    nom STRING,
+    prenom STRING,
+    email STRING,
+    lieu STRING,
+    categorie STRING,
+    mdp STRING,
+    description STRING
+);
