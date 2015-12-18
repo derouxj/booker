e0e5a13f8065fa949b12c046390ed64f0323c591
dyonj <Jichael38@hotmail.fr>
2015-12-18 18:10:55 +0100

    Merge origin/master
    Conflicts:
    	model/DAO.class.php

--- controller/unartiste.php
+++ controller/unartiste.php
@@ -6,7 +6,6 @@ include_once("../model/DAO.class.php");
 $dao = new DAO();
 
 $artiste = $dao->getAllFromUserName($_GET['art'])[0];
-$artiste2 = $dao->getAllFromInfoArtiste($_GET['art'])[0];
 
 
 include("../view/unartiste.view.php");
